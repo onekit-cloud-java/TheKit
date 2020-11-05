82a26c2619c5e64f4cd82f6db55e2c4e4e1816fe
your name <your email>
2020-11-06 01:30:46 +0800

    .

--- src/main/java/cn/onekit/thekit/JSON.java
+++ src/main/java/cn/onekit/thekit/JSON.java
@@ -17,6 +17,9 @@ public class JSON {
     public static JsonElement object2json(Object obj) {
         return new Gson().toJsonTree(obj);
     }
+    public static String object2string(Object obj) {
+        return new Gson().toJson(obj);
+    }
 
     public static <T> T json2object(JsonElement json, Class<T> clazz) {
         return new Gson().fromJson(json, clazz);
