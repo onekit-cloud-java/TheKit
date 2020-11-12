321ed7814b98b9632848f7f435714c749a43df19
your name <your email>
2020-11-12 20:45:47 +0800

    .

--- src/main/java/cn/onekit/thekit/JSON.java
+++ src/main/java/cn/onekit/thekit/JSON.java
@@ -9,19 +9,21 @@ public class JSON {
     public static JsonElement parse(String str) {
         return new JsonParser().parse(str);
     }
-
     public static String stringify(JsonElement json) {
         return json.toString();
     }
-
+    //////////////////////
     public static JsonElement object2json(Object obj) {
         return new Gson().toJsonTree(obj);
     }
+    public static <T> T json2object(JsonElement json, Class<T> clazz) {
+        return new Gson().fromJson(json, clazz);
+    }
+    /////////////////////
     public static String object2string(Object obj) {
         return new Gson().toJson(obj);
     }
-
-    public static <T> T json2object(JsonElement json, Class<T> clazz) {
+    public static <T> T string2object(String json, Class<T> clazz) {
         return new Gson().fromJson(json, clazz);
     }
 }
