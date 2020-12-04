28b80004eac1e12fc98a7f9dc54a325ec9d8f6ac
1093357043 <73806828+1093357043@users.noreply.github.com>
2020-12-04 18:20:28 +0800

    .

--- src/main/java/cn/onekit/thekit/FileDB.java
+++ src/main/java/cn/onekit/thekit/FileDB.java
@@ -2,19 +2,31 @@ package cn.onekit.thekit;
 
 import com.google.gson.JsonObject;
 import java.io.File;
+import java.util.Date;
 
-public class DB {
+public class FileDB {
+    public static class Data{
+        String value;
+        long time;
+    }
     private static String _path(String table){
         return String.format("%s/%s.json",System.getProperties().getProperty("user.home"),table);
     }
-    public static String get(String table,String key) {
+    public static Data get(String table,String key) {
         String path = _path(table);
         JsonObject db;
         if (!new File(path).exists()) {
             return null;
         }
         db = (JsonObject) JSON.parse(FILE.readString(path));
-        return db.has(key) ? db.get(key).getAsString() : null;
+        if( !db.has(key)){
+            return null;
+        }
+        JsonObject json = db.getAsJsonObject(key);
+        Data data = new Data();
+        data.value = json.get("value").getAsString();
+        data.time = json.get("time").getAsLong();
+        return data;
     }
     public static  void set(String table,String key,String value){
         String path = _path(table);
@@ -24,7 +36,10 @@ public class DB {
         }else{
             db = new JsonObject();
         }
-        db.addProperty(key,value);
+        JsonObject data = new JsonObject();
+        data.addProperty("value",value);
+        data.addProperty("time",new Date().getTime());
+        db.add(key,data);
         FILE.writeString(path,JSON.stringify(db));
     }
 }
