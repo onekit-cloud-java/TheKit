1187ad5926acf6e16bea9bacdc761988cafd171f
1093357043 <73806828+1093357043@users.noreply.github.com>
2021-02-23 14:30:15 +0800

    Update STRING.java

--- src/main/java/cn/onekit/thekit/STRING.java
+++ src/main/java/cn/onekit/thekit/STRING.java
@@ -1,8 +1,20 @@
 package cn.onekit.thekit;
 
+import java.util.Map;
+
 @SuppressWarnings("unused")
 public class STRING {
     public static boolean isEmpty(String str){
         return str==null || str.equals("");
+    }
+    public static String format(String format, Map<String,Object> args){
+        String result = new String(format);
+        for(Map.Entry<String,Object> entry : args.entrySet()){
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            String str = String.format("{%s}",key);
+            result.replaceAll(str,value!=null?value.toString():"");
+        }
+        return result;
     }
 }
